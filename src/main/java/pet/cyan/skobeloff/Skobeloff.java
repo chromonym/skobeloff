076c325b471a5e99c2f6104994714a563c32b77c
Cyan <44630816+chromonym@users.noreply.github.com>
2025-01-27 19:54:15 +1030

    one Singular change

--- src/main/java/pet/cyan/skobeloff/Skobeloff.java
+++ src/main/java/pet/cyan/skobeloff/Skobeloff.java
@@ -1,7 +1,9 @@
 package pet.cyan.skobeloff;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -26,7 +28,8 @@ public class Skobeloff implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(content -> {
+			content.add(SALT_PENDANT);
+		});
 	}
 }
